5dc7aef7c6dad548d2f5f6e72c75422e1a6636cc
yusukeagata <yusuke.agata1013@gmail.com>
2016-06-16 17:36:38 +0000

    message-age

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160609031118) do
+ActiveRecord::Schema.define(version: 20160616162257) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
